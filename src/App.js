687692523752163ad9e0d199010fc02d19ea098a
RakanNimer <rakannimer@gmail.com>
2016-12-30 21:02:08 +0200

    Working demo

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Instrument, Note } from 'react-orchestra/web/src/';
+// If you're using react-native then it's :
+// import { Instrument, Note } from 'react-orchestra/native';
+
+const delay = ms => new Promise(resolve => setTimeout(ms, resolve));
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playA: false,
+      playC: false,
+    };
+  }
+  componentDidMount() {
+    this.playMelody();
+  }
+  async playMelody() {
+    await delay(1000);
+    this.setState({ playA: true });
+    await delay(1000);
+    this.setState({ playB: true, playA: false });
+    await delay(1000);
+    this.setState({ playB: false });
+  }
+  render() {
+    return (
+      <Instrument name={'acoustic_grand_piano'} interactive={false}>
+        <Note name={'A3'} play={this.state.playA}>
+          {/*
+            You can put any react element here native or web.
+          */}
+          <div> This is what I want my note to look like ! I can put anything in here.
+            <img
+              alt="Some pic"
+              src="https://s-media-cache-ak0.pinimg.com/originals/36/43/e7/3643e7e8dab9b88b3972ee1c9f909dea.jpg"
+            />
+          </div>
+        </Note>
+        <Note name={'C3'} play={this.state.playC}><div>Another note</div></Note>
+      </Instrument>
+    );
+  }
+}
+export default App;
